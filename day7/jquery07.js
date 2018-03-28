75fd3b825a03f5aae182b5b2627b0a8b0065e432
Rebeca Prestes <rebecaprestes@live.com>
2018-03-28 13:29:19 -0300

    add jquery para o menu

--- day7/jquery07.js
+++ day7/jquery07.js
@@ -1,3 +1,47 @@
 $(window).on('load', function(){
-	alert('jquery funcionando');
+	var sinopse = $('.sinopse');
+	var trailer = $('.trailer');
+	var episodios = $('.episodios');
+	var atores = $('.atores');
+
+	$('#sinopse').click(function(){
+		sinopse.addClass('show');
+		sinopse.removeClass('hide');
+
+		trailer.removeClass('show');
+		trailer.addClass('hide');
+	});
+
+	
+	$('#trailer').click(function(){
+		trailer.addClass('show');
+		trailer.removeClass('hide');
+
+		sinopse.removeClass('show');
+		sinopse.addClass('hide');
+	});
+
+	$('#episodios').click(function(){
+		episodios.addClass('show');
+		episodios.removeClass('hide');
+
+		sinopse.removeClass('show');
+		sinopse.addClass('hide');
+		trailer.removeClass('show');
+		trailer.addClass('hide');
+	});
+
+	$('#atores').click(function(){
+		atores.addClass('show');
+		atores.removeClass('hide');
+
+		sinopse.removeClass('show');
+		sinopse.addClass('hide');
+		trailer.removeClass('show');
+		trailer.addClass('hide');
+		episodios.removeClass('show');
+		episodios.addClass('hide');
+	});
+
+
 });
