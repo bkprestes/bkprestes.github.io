eb0bb37bd785d7307f178b95632a407bb8041df0
Rebeca Prestes <rebecaprestes@live.com>
2018-04-16 16:47:33 -0300

    finalizada a troca das imagens com jquery

--- day4/jquery.js
+++ day4/jquery.js
@@ -1,10 +1,37 @@
 $(function(){
-	$('#preto').click(function(){
-		$(this).attr('src', 'imagens/pixi-preto2.jpg')
-	}).hover(function(){
+
+	$('#preto').hover(function(){
 		$(this).attr('src', 'imagens/pixi-preto2.jpg')
 	},
 	function(){
 		$(this).attr('src', 'imagens/pixi-preto.jpg')
+	});
+
+	$('#branco').hover(function(){
+		$(this).attr('src', 'imagens/pixi-branco2.jpg')
+	},
+	function(){
+		$(this).attr('src', 'imagens/pixi-branco.jpg')
+	});
+
+	$('#vermelho').hover(function(){
+		$(this).attr('src', 'imagens/pixi-vermelho2.jpg')
+	},
+	function(){
+		$(this).attr('src', 'imagens/pixi-vermelho.jpg')
+	});
+
+	$('#azul').hover(function(){
+		$(this).attr('src', 'imagens/pixi-azul2.jpg')
+	},
+	function(){
+		$(this).attr('src', 'imagens/pixi-azul.jpg')
+	});
+
+	$('#todos').hover(function(){
+		$(this).attr('src', 'imagens/pixi-lado.jpg')
+	},
+	function(){
+		$(this).attr('src', 'imagens/pixi-todos.jpg')
 	});
 });
