6cdcd13755f95edc05743a6fb5923219229e132a
Rebeca Prestes <rebecaprestes@live.com>
2018-05-28 15:42:35 -0300

    add options in selected id capital

--- cal-energia-solar/script/arquivo01.js
+++ cal-energia-solar/script/arquivo01.js
@@ -1,2 +1,10 @@
+const capital = document.getElementById('capital')
 
-document.getElementsByName('')
+const arrayCapitais = ['Florianópolis', 'São Paulo', 'Salvador', 'Porto Alegre']
+
+for(let i=0; i<arrayCapitais.length;i++){
+    const options = document.createElement('option')
+    options.innerHTML=arrayCapitais[i]
+    capital.appendChild(options)
+}
+
