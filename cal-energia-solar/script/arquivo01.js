8d8ad1f53672411171b971487297683bbf932322
Rebeca Prestes <rebecaprestes@live.com>
2018-05-29 12:25:10 -0300

    completed dinamic form1 and form2

--- cal-energia-solar/script/arquivo01.js
+++ cal-energia-solar/script/arquivo01.js
@@ -1,12 +1,98 @@
+/*manipulação dos formularios 1 e 2*/
+
 const peca = document.getElementById('peca')
+const formulario1 = document.getElementById('formulario1')
+const formulario2 = document.getElementById('formulario2')
 peca.onchange = function(){
     if(this.value=='ducha-banho' || this.value=='lavatorio' || this.value=='ducha-higienica' || this.value=='banheira' || this.value=='pia-de-cozinha'){
-        const formulario = document.getElementById('formulario')
-        formulario.setAttribute('class', 'show')
-    }else{
-        formulario.setAttribute('class', 'hide')
         
+        formulario2.setAttribute('class', 'hide')
+        formulario1.setAttribute('class', 'show')
+    }else if (this.value=='lava-loucas' || this.value=='maquina-lavar-roupa') {
+        
+        formulario1.setAttribute('class', 'hide')
+        formulario2.setAttribute('class', 'show')
+    } else {
+        formulario1.setAttribute('class', 'hide')
+        formulario2.setAttribute('class', 'hide')
     }
+    
+   
+}
+
+/*manipulação do botão ok referente aos formularios 1 e 2*/
+
+const ok1 = document.getElementById('ok1')
+const ok2 = document.getElementById('ok2')
+
+
+
+ok1.onclick = function(){
+    const vazao1 = document.getElementById('vazao1')
+    const tempoMedio = document.getElementById('tempo-medio-seg-dia')
+    const qtdUso1 = document.getElementById('quantidade-uso-dia1')
+    const tabela1 = document.getElementById('tabela1')
+    const table1 = document.getElementById('table1')
+
+    tabela1.setAttribute('class', 'show')
+    formulario1.setAttribute('class', 'hide')
+    const tr = document.createElement('tr')
+    var td0 = document.createElement('td')
+    var td1 = document.createElement('td')
+    var td2 = document.createElement('td')  
+    var td3 = document.createElement('td')      
+    
+    td0.innerHTML = peca.value
+    td1.innerHTML = vazao1.value
+    td2.innerHTML = tempoMedio.value
+    td3.innerHTML = qtdUso1.value
+
+    tr.appendChild(td0)
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+
+    table1.appendChild(tr)
+
+    document.getElementById('escolha').selected = true
+
+    vazao1.value = ''
+    tempoMedio.value = ''
+    qtdUso1.value =''
+    return false
+
+
+}
+
+ok2.onclick = function(){
+    const vazao2 = document.getElementById('vazao2')    
+    const qtdUso2 = document.getElementById('quantidade-uso-dia2')
+    const tabela2 = document.getElementById('tabela2')
+    const table2 = document.getElementById('table2')
+
+    tabela2.setAttribute('class', 'show')
+    formulario2.setAttribute('class', 'hide')
+    const tr = document.createElement('tr')
+    var td0 = document.createElement('td')
+    var td1 = document.createElement('td')
+    var td2 = document.createElement('td')  
+          
+    
+    td0.innerHTML = peca.value
+    td1.innerHTML = vazao2.value
+    td2.innerHTML = qtdUso2.value
+
+    tr.appendChild(td0)
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+
+    table2.appendChild(tr)
+
+    document.getElementById('escolha').selected = true
+
+    vazao2.value = ''
+    qtdUso2.value =''
+    return false
 }
 
 
