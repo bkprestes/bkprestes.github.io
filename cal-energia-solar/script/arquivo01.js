f6502a5ff36678d2c3496444cae370de7360b7d9
Rebeca Prestes <rebecaprestes@live.com>
2018-05-29 16:07:35 -0300

    add verificacao de campos que nao podem ficar vazios form1 e form2

--- cal-energia-solar/script/arquivo01.js
+++ cal-energia-solar/script/arquivo01.js
@@ -24,74 +24,89 @@ peca.onchange = function(){
 
 const ok1 = document.getElementById('ok1')
 const ok2 = document.getElementById('ok2')
+const vazao1 = document.getElementById('vazao1')
+const tempoMedio = document.getElementById('tempo-medio-seg-dia')
+const qtdUso1 = document.getElementById('quantidade-uso-dia1')
+const tabela1 = document.getElementById('tabela1')
+const table1 = document.getElementById('table1')
+
+/*verifica se campos estão vazios*/
 
 
 
 ok1.onclick = function(){
-    const vazao1 = document.getElementById('vazao1')
-    const tempoMedio = document.getElementById('tempo-medio-seg-dia')
-    const qtdUso1 = document.getElementById('quantidade-uso-dia1')
-    const tabela1 = document.getElementById('tabela1')
-    const table1 = document.getElementById('table1')
-
-    tabela1.setAttribute('class', 'show')
-    formulario1.setAttribute('class', 'hide')
-    const tr = document.createElement('tr')
-    var td0 = document.createElement('td')
-    var td1 = document.createElement('td')
-    var td2 = document.createElement('td')  
-    var td3 = document.createElement('td')      
     
-    td0.innerHTML = peca.value
-    td1.innerHTML = vazao1.value
-    td2.innerHTML = tempoMedio.value
-    td3.innerHTML = qtdUso1.value
+    if(tempoMedio.value=='' || qtdUso1.value==''){
+        alert('Não deixe os campos vazios, com exceção do campo Vazão.')
+    }else{
+        tabela1.setAttribute('class', 'show')
+        formulario1.setAttribute('class', 'hide')
+        const tr = document.createElement('tr')
+        var td0 = document.createElement('td')
+        var td1 = document.createElement('td')
+        var td2 = document.createElement('td')  
+        var td3 = document.createElement('td')      
+        
+        td0.innerHTML = peca.value
+        td1.innerHTML = vazao1.value  
+        td2.innerHTML = tempoMedio.value
+        td3.innerHTML = qtdUso1.value
+        
 
-    tr.appendChild(td0)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tr.appendChild(td3)
+        tr.appendChild(td0)
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
 
-    table1.appendChild(tr)
+        table1.appendChild(tr)
 
-    document.getElementById('escolha').selected = true
+        document.getElementById('escolha').selected = true
 
-    vazao1.value = ''
-    tempoMedio.value = ''
-    qtdUso1.value =''
-    return false
+        vazao1.value = ''
+        tempoMedio.value = ''
+        qtdUso1.value =''
+        
+        }
 
-
+        return false
 }
 
-ok2.onclick = function(){
     const vazao2 = document.getElementById('vazao2')    
     const qtdUso2 = document.getElementById('quantidade-uso-dia2')
     const tabela2 = document.getElementById('tabela2')
     const table2 = document.getElementById('table2')
 
-    tabela2.setAttribute('class', 'show')
-    formulario2.setAttribute('class', 'hide')
-    const tr = document.createElement('tr')
-    var td0 = document.createElement('td')
-    var td1 = document.createElement('td')
-    var td2 = document.createElement('td')  
-          
+ok2.onclick = function(){
     
-    td0.innerHTML = peca.value
-    td1.innerHTML = vazao2.value
-    td2.innerHTML = qtdUso2.value
 
-    tr.appendChild(td0)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
+    if(qtdUso2.value==''){
+        alert('Não deixe os campos vazios, com exceção do campo Vazão.')
+    }else{
+        tabela2.setAttribute('class', 'show')
+        formulario2.setAttribute('class', 'hide')
+        const tr = document.createElement('tr')
+        var td0 = document.createElement('td')
+        var td1 = document.createElement('td')
+        var td2 = document.createElement('td')  
+            
+        
+        td0.innerHTML = peca.value
+        td1.innerHTML = vazao2.value
+        td2.innerHTML = qtdUso2.value
 
-    table2.appendChild(tr)
+        tr.appendChild(td0)
+        tr.appendChild(td1)
+        tr.appendChild(td2)
 
-    document.getElementById('escolha').selected = true
+        table2.appendChild(tr)
 
-    vazao2.value = ''
-    qtdUso2.value =''
+        document.getElementById('escolha').selected = true
+
+        vazao2.value = ''
+        qtdUso2.value =''   
+    }
+
+    
     return false
 }
 
