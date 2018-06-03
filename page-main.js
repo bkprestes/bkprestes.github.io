5630ae96f5a9c417e6310b4eaa43018f3d5e8bb6
Rebeca Prestes <rebecaprestes@live.com>
2018-06-03 16:56:52 -0300

    update navbar position fixed

--- page-main.js
+++ page-main.js
@@ -3,3 +3,22 @@ $('.img-fluid').hover(function(){
 },function(){
     $(this).removeClass('img-maior')
 })
+
+const nav = document.querySelector('[data-animacao]')
+const main = document.getElementById('main')
+
+function animeScroll(){
+    const windowTop = window.pageYOffset
+    console.log(windowTop)
+    if(windowTop>150){
+        nav.classList.add('navFixed')        
+        main.classList.add('main')
+    }else{
+        nav.classList.remove('navFixed')
+        main.classList.remove('main')
+    }
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll()
+})
