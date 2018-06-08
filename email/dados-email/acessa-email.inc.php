32bd48cd8b7d304f093b90894e19e3728c6cc360
Rebeca Prestes <rebecaprestes@live.com>
2018-06-08 18:41:17 -0300

    instrucao atualizada

--- email/dados-email/acessa-email.inc.php
+++ email/dados-email/acessa-email.inc.php
@@ -2,6 +2,6 @@
 	//dados das variáveis de acesso ao e-mail
 	// o mais recomendável é trocar os nomes dessas variáveis para disfarçar.
 	
-	$usuario = "remetente.ifsc2016@gmail.com";
-	$senha = "ifsc1234";
+	$usuario = "minhacontadeteste07@gmail.com";
+	$senha = "rebeca07";
 ?>
