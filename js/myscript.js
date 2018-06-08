9763ec9a3f21977b79f527bd42d2ca9d51ca8094
Rebeca Prestes <rebecaprestes@live.com>
2018-06-08 15:27:12 -0300

    add js in menu

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,9 @@
 
 const links = document.getElementById('links');
-const linklink=document.getElementById('network');
+const linklink = document.getElementById('network');
 const network = document.getElementById('menu-network');
-function redimensionar(){
+
+function verificarTamanho(){
     if(window.innerWidth>768){
         linklink.appendChild(links);
         network.setAttribute('class', 'hide');
@@ -11,9 +12,28 @@ function redimensionar(){
     }
 }
 
+
+
+function redimensionar(){
+    verificarTamanho();
+}
+
+verificarTamanho();
 window.addEventListener('resize', function(){
     redimensionar();
 });
+
+const menu = document.getElementById('lista-menu').getElementsByTagName('a');
+
+for(let i=0; i<4; i++){
+    menu[i].onmouseover = function(){
+        menu[i].innerHTML = menu[i].getAttribute('data-traducao')
+    }
+    menu[i].onmouseout = function(){
+        menu[i].innerHTML = menu[i].getAttribute('data-original')
+    }
+}
+
 
 
 
